a66a3b539596bfc5ef4751eb60980ac77be2ea6e
fomi4ok <32990229+fomi4ok@users.noreply.github.com>
2018-02-06 22:51:22 -0600

    login test

--- java_example/src/test/java/ru/stqa/school/selenium/MyFirstTest.java
+++ java_example/src/test/java/ru/stqa/school/selenium/MyFirstTest.java
@@ -3,6 +3,7 @@ package ru.stqa.school.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -23,7 +24,12 @@ public class MyFirstTest {
   @Test
 
   public void myFirstTest() {
-    driver.get("http://www.google.com");
+
+    driver.get(" http://localhost/litecart/admin/");
+    driver.findElement(By.name("username")).sendKeys("admin");
+    driver.findElement(By.name("password")).sendKeys("admin");
+    driver.findElement(By.name("login")).click();
+
   }
 
   @After
